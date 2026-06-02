afd2676af279f2ada75f431f2a6501755408bb4a
Ashutoshpalai3073 <ashutosh.palai2005@gmail.com>
2026-06-02 08:01:08 +0530

    fuctions of the deployment

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,4 +1,4 @@
 export async function onRequest(context: any) {
-  const { default: handler } = await import('../dist/server/index.js')
+  const { default: handler } = await import('../dist/serve/server.js')
   return handler.fetch(context.request, context.env, context)
 }
