3b72114f2123615ec41c7407a6fcbaa4a1306cd0
Ashutoshpalai3073 <ashutosh.palai2005@gmail.com>
2026-06-02 09:21:27 +0530

    more and more work

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL  as string;
-const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const url = import.meta.env.VITE_SUPABASE_URL || 'https://kntoyozitskrblvxmbpp.supabase.co';
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtudG95b3ppdHNrcmJsdnhtYnBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4Njg1ODMsImV4cCI6MjA5NTQ0NDU4M30.o1nTOoJ4BPKrr95WAuqYa3FfDwIhjj10R5Ra7eBVGok';
 
 export const supabase = createClient(url, key);
 
